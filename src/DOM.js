d82dda1747b751ef0625900674528687a90c06ab
JosueDeLosSantos <jdd1993@hotmail.com>
2023-05-23 16:36:22 -0400

    Add first grid.

--- src/DOM.js
+++ src/DOM.js
@@ -3,10 +3,12 @@ import "./style.css";
 const body = document.querySelector("body");
 const header = document.createElement("header");
 const main = document.createElement("main");
+const grids = document.createElement("div");
 const footer = document.createElement("footer");
 
 body.appendChild(header);
 body.appendChild(main);
+main.appendChild(grids);
 body.appendChild(footer);
 
 /* Create header containing the game name and a box for game alerts */
@@ -15,7 +17,7 @@ body.appendChild(footer);
 // header
 const gameTitle = document.createElement("div");
 const title = document.createElement("div");
-title.classList.add("gTitle");
+title.classList.add("gameTitle");
 title.textContent = "Battleship";
 gameTitle.appendChild(title);
 const notificationBar = document.createElement("div");
@@ -26,6 +28,21 @@ notificationBar.appendChild(bar);
 header.appendChild(gameTitle);
 header.appendChild(notificationBar);
 
-/* Create the player grid */
-
 /* Create the PC grid */
+
+const playerGrid = document.createElement("div");
+playerGrid.classList.add("playerGrid");
+grids.appendChild(playerGrid);
+for (let i = 0; i < 10; i += 1) {
+  const gridRow = document.createElement("div");
+  gridRow.dataset.row = `${i}`;
+  playerGrid.appendChild(gridRow);
+  for (let j = 0; j < 10; j += 1) {
+    const gridField = document.createElement("div");
+    gridField.dataset.field = `${i},${j}`;
+    const gridRowSelector = document.querySelector(`[data-row = "${i}"]`);
+    gridRowSelector.appendChild(gridField);
+  }
+}
+
+/* Create the player grid */
