963fa3c4fc44725d39a5ec7f966ba14e0c3942b7
JosueDeLosSantos <jdd1993@hotmail.com>
2023-06-19 22:08:49 -0400

    Finish clear1 function

--- src/logic.js
+++ src/logic.js
@@ -183,6 +183,70 @@ function clear1(arr, matrix) {
       newArr.push(matrix.board[arr[0][0] + 1][arr[0][1] + 1]);
   }
 
+  if ((arr[0][0] === 0) && (arr[0][1] === 0)){
+    newArr.push(matrix.board[arr[0][0]][arr[0][1] + 1]);
+    newArr.push(matrix.board[arr[0][0] + 1][arr[0][1]]);
+    newArr.push(matrix.board[arr[0][0] + 1][arr[0][1] + 1]);
+  }
+
+  if ((arr[0][0] === 0) && (arr[0][1] === 9)){
+    newArr.push(matrix.board[arr[0][0]][arr[0][1] - 1]);
+    newArr.push(matrix.board[arr[0][0] + 1][arr[0][1]]);
+    newArr.push(matrix.board[arr[0][0] + 1][arr[0][1] - 1]);
+  }
+
+  if ((arr[0][0] === 9) && (arr[0][1] === 0)){
+    newArr.push(matrix.board[arr[0][0] - 1][arr[0][1]]);
+    newArr.push(matrix.board[arr[0][0] - 1][arr[0][1] + 1]);
+    newArr.push(matrix.board[arr[0][0]][arr[0][1] + 1]);
+  }
+
+  if ((arr[0][0] === 9) && (arr[0][1] === 9)){
+    newArr.push(matrix.board[arr[0][0] - 1][arr[0][1]]);
+    newArr.push(matrix.board[arr[0][0] - 1][arr[0][1] - 1]);
+    newArr.push(matrix.board[arr[0][0]][arr[0][1] - 1]);
+  }
+
+  if ((arr[0][0] === 0) && 
+  (arr[0][1] > 0) &&
+  (arr[0][1] < 9)){
+    newArr.push(matrix.board[arr[0][0]][arr[0][1] - 1]);
+    newArr.push(matrix.board[arr[0][0] + 1][arr[0][1] - 1]);
+    newArr.push(matrix.board[arr[0][0] + 1][arr[0][1]]);
+    newArr.push(matrix.board[arr[0][0] + 1][arr[0][1] + 1]);
+    newArr.push(matrix.board[arr[0][0]][arr[0][1] + 1]);
+  }
+
+  if ((arr[0][0] === 9) && 
+  (arr[0][1] > 0) &&
+  (arr[0][1] < 9)){
+    newArr.push(matrix.board[arr[0][0]][arr[0][1] - 1]);
+    newArr.push(matrix.board[arr[0][0] - 1][arr[0][1] - 1]);
+    newArr.push(matrix.board[arr[0][0] - 1][arr[0][1]]);
+    newArr.push(matrix.board[arr[0][0] - 1][arr[0][1] + 1]);
+    newArr.push(matrix.board[arr[0][0]][arr[0][1] + 1]);
+  }
+
+  if ((arr[0][1] === 0) && 
+  (arr[0][0] > 0) &&
+  (arr[0][0] < 9)){
+    newArr.push(matrix.board[arr[0][0] - 1][arr[0][1]]);
+    newArr.push(matrix.board[arr[0][0] - 1][arr[0][1] + 1]);
+    newArr.push(matrix.board[arr[0][0]][arr[0][1] + 1]);
+    newArr.push(matrix.board[arr[0][0] + 1][arr[0][1]]);
+    newArr.push(matrix.board[arr[0][0] + 1][arr[0][1] + 1]);
+  }
+
+  if ((arr[0][1] === 9) && 
+  (arr[0][0] > 0) &&
+  (arr[0][0] < 9)){
+    newArr.push(matrix.board[arr[0][0] - 1][arr[0][1]]);
+    newArr.push(matrix.board[arr[0][0] - 1][arr[0][1] - 1]);
+    newArr.push(matrix.board[arr[0][0]][arr[0][1] - 1]);
+    newArr.push(matrix.board[arr[0][0] + 1][arr[0][1]]);
+    newArr.push(matrix.board[arr[0][0] + 1][arr[0][1] - 1]);
+  }
+
   newArr.forEach((el) => {
     if (el.length > 0) {
       noEmptyArr.push(el);
